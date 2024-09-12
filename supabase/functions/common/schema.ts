6c8197447a272d9642cf03216935b9a028131e1c
Turbobot <turbobot@vercel.com>
2024-09-12 21:14:48 +0900

    feat(create-turbo): apply official-starter transform

--- supabase/functions/common/schema.ts
+++
@@ -1,8 +0,0 @@
-import { pgTable, serial, text, integer } from "drizzle-orm/pg-core";
-
-export const usersTable = pgTable('users_table', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  age: integer('age').notNull(),
-  email: text('email').notNull().unique(),
-})
